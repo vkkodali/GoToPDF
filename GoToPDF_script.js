40b3fe39bcff47c580b7e5cc495c68467f9af994
vkkodali <vkkodali@gmail.com>
2016-05-13 15:40:17 -0400

    Update and rename alert.js to GoToPDF_script.js

---
+++ GoToPDF_script.js
@@ -0,0 +1,22 @@
+javascript:(function(){
+	var theUrl = window.location.href;
+	if (theUrl.startsWith("http://www.jbc.org/content") || 
+	    theUrl.startsWith("http://jcb.rupress.org/content/") || 
+	    theUrl.startsWith("http://emboj.embopress.org/content/") || 
+	    theUrl.startsWith("http://jcs.biologists.org/content/") || 
+	    theUrl.startsWith("http://mcb.asm.org/content/") || 
+	    theUrl.startsWith("http://circ.ahajournals.org/content/") ){
+		theUrl = theUrl.replace(".long", "");
+		window.open(theUrl + '.full.pdf', "_self");
+	} else if (theUrl.startsWith("http://onlinelibrary.wiley.com/doi/")) {
+		var garbage = theUrl.indexOf('?');
+		theUrl = theUrl.substring(0, garbage != -1 ? garbage : theUrl.length);
+		window.open(theUrl.replace('abstract', 'pdf'), "_self");
+	} else if (theUrl.startsWith("http://press.endocrine.org/doi/")){
+		var garbage = theUrl.indexOf('?');
+		theUrl = theUrl.substring(0, garbage != -1 ? garbage : theUrl.length);
+		window.open(theUrl.replace('doi', 'doi/pdf'), "_self" );
+	}	else {
+		alert("Don't know what's going on here!")
+	}
+})();

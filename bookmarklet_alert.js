bc96217e75b563874ccbdf8c8f7a6f3220fc8d7a
vkkodali <vkkodali@gmail.com>
2016-05-13 15:33:09 -0400

    Update bookmarklet_alert.js

--- bookmarklet_alert.js
+++ bookmarklet_alert.js
@@ -1,5 +1,5 @@
 javascript: (function () { 
   var jsCode = document.createElement('script'); 
-  jsCode.setAttribute('src', '');
+  jsCode.setAttribute('src', 'http://cdn.rawgit.com/vkkodali/alert.js/master/alert.js');
 	document.body.appendChild(jsCode); 
  }());
